3b19b1a5788bd4b64c5542c1277fca348b0e6edf
marinainsaurralde <marinainsaurralde11@gmail.com>
2017-06-28 14:32:34 -0400

    Initial commit

---
+++ app/src/main/java/fiona/checker/Checker.java
@@ -0,0 +1,13 @@
+package fiona.checker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Checker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checker);
+    }
+}
